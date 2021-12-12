ab54f7c3d5bfc6357631e42605ec3ae2890c758d
SLIMANI <abdennour.slimani@outlook.fr>
2021-12-12 10:34:03 +0100

    first commti of project

---
+++ src/main/java/com/mycompany/roadtripplanner/RoadtripplannerApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.roadtripplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadtripplannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadtripplannerApplication.class, args);
+    }
+
+}
