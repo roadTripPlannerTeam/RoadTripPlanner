0892c62abda7e76d5cbf7d4d518761a2b0fa3ba0
Brice Dobrowolski <brice.dobrowolski@yahoo.fr>
2021-12-23 12:09:30 +0100

    fonction calculateNewAmount() dans le fichier Util

--- src/main/java/com/mycompany/roadtripplanner/RoadtripplannerApplication.java
+++ src/main/java/com/mycompany/roadtripplanner/RoadtripplannerApplication.java
@@ -1,5 +1,6 @@
 package com.mycompany.roadtripplanner;
 
+import com.mycompany.roadtripplanner.utils.Util;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/com/mycompany/roadtripplanner/utils/Util.java
@@ -0,0 +1,19 @@
+package com.mycompany.roadtripplanner.utils;
+
+public class Util {
+
+    /**
+     * Fonction qui permet de calculer un nouveau montant
+     * @Param double initalAmout, double expense
+     * @Return double newAmount
+     */
+    public static double calculateNewAmount(double initialAmount, double expense){
+        double newAmount = initialAmount - expense;
+        if(newAmount < 0){
+            newAmount = 0;
+        }
+        System.out.println(newAmount);
+        return newAmount;
+    }
+
+}
