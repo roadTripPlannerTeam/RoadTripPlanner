e02506dea981bde82cbbbd4c28f81f43ff426702
Brice Dobrowolski <brice.dobrowolski@yahoo.fr>
2021-12-23 11:44:41 +0100

    conversion deleted

--- src/main/java/com/mycompany/roadtripplanner/repositories/ConversionRepositoryImpl.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.roadtripplanner.repositories;
-
-import com.mycompany.roadtripplanner.entities.Conversion;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ConversionRepositoryImpl extends MongoRepository<Conversion, String> {
-}
