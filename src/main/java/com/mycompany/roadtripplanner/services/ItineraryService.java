06ce4c759fa4d1d78d7bb843388faf864311d562
SLIMANI <abdennour.slimani@outlook.fr>
2021-12-26 20:13:41 +0100

    fix:[Itinerary] refactor save methode

--- src/main/java/com/mycompany/roadtripplanner/services/ItineraryService.java
+++ src/main/java/com/mycompany/roadtripplanner/services/ItineraryService.java
@@ -1,5 +1,6 @@
 package com.mycompany.roadtripplanner.services;
 
+import com.mycompany.roadtripplanner.dtos.itinearay.ItenaryGetSaveDTO;
 import com.mycompany.roadtripplanner.dtos.itinearay.ItineraryDTO;
 import com.mycompany.roadtripplanner.dtos.itinearay.ItinerarySaveDTO;
 import com.mycompany.roadtripplanner.dtos.itinearay.ItineraryUpdateDTO;
@@ -30,14 +31,12 @@ public class ItineraryService {
 
     /**
      *
-     * @param itinerary
+     * @param itinerarySaveDTO
      * @return
      */
-    public ItineraryDTO save(ItinerarySaveDTO itineraryObj) {
-        Itinerary itineraryToSave = mapper.map(itineraryObj,Itinerary.class);
-        Itinerary itinerary =repository.save(itineraryToSave);
-        ItineraryDTO itinerarySaved = mapper.map(itinerary, ItineraryDTO.class);
-        return itinerarySaved;
+    public ItenaryGetSaveDTO save(ItinerarySaveDTO itinerarySaveDTO) {
+      return   mapper.map(repository.save(mapper.map(itinerarySaveDTO,Itinerary.class)), ItenaryGetSaveDTO.class);
+
     }
 
     /**
