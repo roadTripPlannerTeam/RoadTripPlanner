8f64730386336c4b79e7542190d0da0c98658057
SLIMANI <abdennour.slimani@outlook.fr>
2021-12-27 20:39:55 +0100

    itiniraryDTO

---
+++ src/main/java/com/mycompany/roadtripplanner/dtos/itinearay/ItineraryGetSaveDTO.java
@@ -0,0 +1,23 @@
+package com.mycompany.roadtripplanner.dtos.itinearay;
+
+import com.mycompany.roadtripplanner.dtos.budget.BudgetDTO;
+import com.mycompany.roadtripplanner.dtos.budget.BudgetRelationDTO;
+import com.mycompany.roadtripplanner.dtos.todolist.TodoListDTO;
+import com.mycompany.roadtripplanner.dtos.todolist.TodoListRelationDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItineraryGetSaveDTO {
+    private String id;
+    private String title;
+    private String experienceFeedback;
+    private float nbStars;
+    private TodoListRelationDTO todoList;
+    private BudgetRelationDTO budget;
+    private ItineraryRelationDTO itinerary;
+}
---
+++ src/main/java/com/mycompany/roadtripplanner/dtos/todolist/TodoListRelationDTO.java
@@ -0,0 +1,14 @@
+package com.mycompany.roadtripplanner.dtos.todolist;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoListRelationDTO {
+    private String id ;
+
+}
