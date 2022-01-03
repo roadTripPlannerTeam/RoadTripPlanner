6af50afb9ea0892d1c7e3405dc4ece6c126899b8
Brice Dobrowolski <brice.dobrowolski@yahoo.fr>
2022-01-03 19:05:40 +0100

    fix [Api UnknownStage] correction UnknownStageDTOs

--- src/main/java/com/mycompany/roadtripplanner/dtos/unknownstage/UnknownStageDTO.java
+++ src/main/java/com/mycompany/roadtripplanner/dtos/unknownstage/UnknownStageDTO.java
@@ -1,18 +1,19 @@
 package com.mycompany.roadtripplanner.dtos.unknownstage;
 
 import com.mycompany.roadtripplanner.dtos.position.PositionRelationDTO;
+import com.mycompany.roadtripplanner.entities.InterestPoint;
+import com.mycompany.roadtripplanner.entities.Stage;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UnknownStageDTO {
-
     private String id;
-    private String name;
-    private PositionRelationDTO position;
-    private String category;
-
+    private Stage stage;
+    private List<InterestPoint> interestPoints;
 }
--- src/main/java/com/mycompany/roadtripplanner/dtos/unknownstage/UnknownStageSaveDTO.java
+++ src/main/java/com/mycompany/roadtripplanner/dtos/unknownstage/UnknownStageSaveDTO.java
@@ -1,16 +1,18 @@
 package com.mycompany.roadtripplanner.dtos.unknownstage;
 
 import com.mycompany.roadtripplanner.dtos.position.PositionRelationDTO;
+import com.mycompany.roadtripplanner.entities.InterestPoint;
+import com.mycompany.roadtripplanner.entities.Stage;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UnknownStageSaveDTO {
-
-    private String name;
-    private PositionRelationDTO position;
-    private String category;
+    private Stage stage;
+    private List<InterestPoint> interestPoints;
 }
